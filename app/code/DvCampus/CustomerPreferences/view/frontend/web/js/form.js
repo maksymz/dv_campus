9ccfab878d9a7beb119dc21fb70e3b1f00bb51e4
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2019-12-17 01:45:45 +0200

    Added form submit handler with AJAX, but without a backend controller

--- app/code/DvCampus/CustomerPreferences/view/frontend/web/js/form.js
+++ app/code/DvCampus/CustomerPreferences/view/frontend/web/js/form.js
@@ -1,7 +1,8 @@
 define([
     'jquery',
+    'Magento_Ui/js/modal/alert',
     'Magento_Ui/js/modal/modal'
-], function ($) {
+], function ($, alert) {
     'use strict';
 
     $.widget('dvCampusCustomerPreferences.form', {
@@ -27,9 +28,60 @@ define([
         },
 
         savePreferences: function () {
-            alert('saved!');
-            return false;
-        }
+            if (!this.validateForm()) {
+                return;
+            }
+
+            this.ajaxSubmit();
+        },
+
+        /**
+         * Validate request form
+         */
+        validateForm: function () {
+            return $(this.element).validation().valid();
+        },
+
+        /**
+         * Submit request via AJAX. Add form key to the post data.
+         */
+        ajaxSubmit: function () {
+            var formData = new FormData($(this.element).get(0));
+
+            formData.append('form_key', $.mage.cookies.get('form_key'));
+            formData.append('isAjax', 1);
+
+            $.ajax({
+                url: this.options.action,
+                data: formData,
+                processData: false,
+                contentType: false,
+                type: 'post',
+                dataType: 'json',
+                context: this,
+
+                /** @inheritdoc */
+                beforeSend: function () {
+                    $('body').trigger('processStart');
+                },
+
+                /** @inheritdoc */
+                success: function (response) {
+                    $('body').trigger('processStop');
+                    // @TODO: show new preferences
+                },
+
+                /** @inheritdoc */
+                error: function () {
+                    $('body').trigger('processStop');
+                    alert({
+                        title: $.mage.__('Error'),
+                        /*eslint max-len: ["error", { "ignoreStrings": true }]*/
+                        content: $.mage.__('Your preferences can\'t be saved. Please, contact us if ypu see this message.')
+                    });
+                }
+            });
+        },
     });
 
     return $.dvCampusCustomerPreferences.form;
