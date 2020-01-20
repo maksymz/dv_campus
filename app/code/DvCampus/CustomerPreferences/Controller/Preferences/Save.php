e1a5a517c007e4b4f64dddfdada464a2eff59bc5
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2020-01-20 22:39:35 +0200

    Lesson 20: working with customer session to save preferences

--- app/code/DvCampus/CustomerPreferences/Controller/Preferences/Save.php
+++ app/code/DvCampus/CustomerPreferences/Controller/Preferences/Save.php
@@ -8,10 +8,21 @@ use DvCampus\CustomerPreferences\Model\ResourceModel\Preference\Collection as Pr
 use Magento\Framework\Controller\Result\Json as JsonResult;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\DB\Transaction;
+use Magento\Framework\Exception\LocalizedException;
 
 class Save extends \Magento\Framework\App\Action\Action implements
     \Magento\Framework\App\Action\HttpPostActionInterface
 {
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    private $customerSession;
+
+    /**
+     * @var \Magento\Framework\Data\Form\FormKey\Validator
+     */
+    private $formKeyValidator;
+
     /**
      * @var \DvCampus\CustomerPreferences\Model\PreferenceFactory $preferenceFactory
      */
@@ -39,6 +50,9 @@ class Save extends \Magento\Framework\App\Action\Action implements
 
     /**
      * Save constructor.
+     *
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator
      * @param \DvCampus\CustomerPreferences\Model\PreferenceFactory $preferenceFactory
      * @param \DvCampus\CustomerPreferences\Model\ResourceModel\Preference\CollectionFactory $preferenceCollectionFactory
      * @param \Magento\Framework\DB\TransactionFactory $transactionFactory
@@ -47,6 +61,8 @@ class Save extends \Magento\Framework\App\Action\Action implements
      * @param \Magento\Framework\App\Action\Context $context
      */
     public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
         \DvCampus\CustomerPreferences\Model\PreferenceFactory $preferenceFactory,
         \DvCampus\CustomerPreferences\Model\ResourceModel\Preference\CollectionFactory $preferenceCollectionFactory,
         \Magento\Framework\DB\TransactionFactory $transactionFactory,
@@ -55,6 +71,8 @@ class Save extends \Magento\Framework\App\Action\Action implements
         \Magento\Framework\App\Action\Context $context
     ) {
         parent::__construct($context);
+        $this->customerSession = $customerSession;
+        $this->formKeyValidator = $formKeyValidator;
         $this->preferenceFactory = $preferenceFactory;
         $this->preferenceCollectionFactory = $preferenceCollectionFactory;
         $this->transactionFactory = $transactionFactory;
@@ -67,50 +85,67 @@ class Save extends \Magento\Framework\App\Action\Action implements
      */
     public function execute()
     {
-        // @TODO: implement security layer when we get back to JS
-        // @TODO: save data to customer session for guests
-        /** @var Transaction $transaction */
-        $transaction = $this->transactionFactory->create();
+        // @TODO: merge with customer preferences on login or not? Maybe merge only if empty?
+        $request = $this->getRequest();
 
         // Every fail should be controlled
         try {
-            $websiteId = (int) $this->storeManager->getWebsite()->getId();
-            $preferencesByAttributeCode = [];
-
-            /** @var PreferenceCollection $preferenceCollection */
-            $preferenceCollection = $this->preferenceCollectionFactory->create();
-            // @TODO: get `customer_id` from the session
-            $preferenceCollection->addCustomerFilter(1)
-                ->addWebsiteFilter($websiteId);
-
-            /** @var Preference $existingPreference */
-            foreach ($preferenceCollection as $existingPreference) {
-                $preferencesByAttributeCode[$existingPreference->getAttributeCode()] = $existingPreference;
+            if (!$this->formKeyValidator->validate($request)) {
+                // This message is translated in the module Magento_Checkout
+                throw new LocalizedException(__('Your session has expired'));
             }
 
-            foreach ($this->getRequest()->getParam('attributes') as $attributeCode => $value) {
-                if (isset($preferencesByAttributeCode[$attributeCode])) {
-                    $preference = $preferencesByAttributeCode[$attributeCode];
+            $customerId = (int) $this->customerSession->getId();
+            $websiteId = (int) $this->storeManager->getWebsite()->getId();
 
-                    if ($preference->getPreferredValues() !== $value) {
-                        $preference->setPreferredValues($value);
+            if ($customerId) {
+                $preferencesByAttributeCode = [];
+
+                /** @var PreferenceCollection $preferenceCollection */
+                $preferenceCollection = $this->preferenceCollectionFactory->create();
+                $preferenceCollection->addCustomerFilter($customerId)
+                    ->addWebsiteFilter($websiteId);
+
+                /** @var Preference $existingPreference */
+                foreach ($preferenceCollection as $existingPreference) {
+                    $preferencesByAttributeCode[$existingPreference->getAttributeCode()] = $existingPreference;
+                }
+
+                /** @var Transaction $transaction */
+                $transaction = $this->transactionFactory->create();
+
+                foreach ($request->getParam('attributes') as $attributeCode => $value) {
+                    if (isset($preferencesByAttributeCode[$attributeCode])) {
+                        $preference = $preferencesByAttributeCode[$attributeCode];
+
+                        if ($preference->getPreferredValues() !== $value) {
+                            $preference->setPreferredValues($value);
+                            $transaction->addObject($preference);
+                        }
+                    } else {
+                        /** @var Preference $preference */
+                        $preference = $this->preferenceFactory->create();
+
+                        $preference->setCustomerId($customerId)
+                            ->setWebsiteId($websiteId)
+                            ->setAttributeId($attributeCode)
+                            ->setPreferredValues($value);
                         $transaction->addObject($preference);
                     }
-                } else {
-                    /** @var Preference $preference */
-                    $preference = $this->preferenceFactory->create();
-
-                    // @TODO: get `customer_id` from the session
-                    $preference->setCustomerId(1)
-                        ->setWebsiteId($websiteId)
-                        ->setAttributeId($attributeCode)
-                        ->setPreferredValues($value);
-                    $transaction->addObject($preference);
                 }
-            }
 
-            $transaction->save();
-            $message = __('Saved!');
+                $transaction->save();
+                $message = __('Your preferences have been updated.');
+            } else {
+                $preferencesByAttributeCode = array_merge(
+                    $this->customerSession->getData('customer_preferences') ?? [],
+                    $request->getParam('attributes')
+                );
+                $this->customerSession->setCustomerPreferences($preferencesByAttributeCode);
+                $message = __('Your preferences have been updated. Please, log in to save them permanently.');
+            }
+        } catch (LocalizedException $e) {
+            $message = $e->getMessage();
         } catch (\Exception $e) {
             $this->logger->critical($e);
             $message = __('Your preferences can\'t be saved. Please, contact us if ypu see this message.');
--- app/etc/env.php
+++ app/etc/env.php
@@ -29,7 +29,27 @@ return [
     'x-frame-options' => 'SAMEORIGIN',
     'MAGE_MODE' => 'developer',
     'session' => [
-        'save' => 'files'
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '3',
+            'max_concurrency' => '6',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '1',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000'
+        ]
     ],
     'cache' => [
         'frontend' => [
@@ -44,7 +64,7 @@ return [
     'lock' => [
         'provider' => 'db',
         'config' => [
-            'prefix' => null
+            'prefix' => ''
         ]
     ],
     'cache_types' => [
@@ -62,10 +82,11 @@ return [
         'full_page' => 0,
         'config_webservice' => 1,
         'translate' => 1,
-        'vertex' => 1
+        'vertex' => 1,
+        'google_product' => 1
     ],
     'install' => [
-        'date' => 'Sat, 05 Oct 2019 08:52:04 +0000'
+        'date' => 'Mon, 23 Dec 2019 23:11:04 +0000'
     ],
     'downloadable_domains' => [
         'demo-232.local',
