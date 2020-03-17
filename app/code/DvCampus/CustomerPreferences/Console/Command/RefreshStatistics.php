aa07a48867186a81dc0d374199280f9ed0affdc7
Maksym Zaporozhets <maksimz@default-value.com>
2020-03-17 14:35:49 +0200

    Lesson 29: Emulate area "adminhtml" in the command

--- app/code/DvCampus/CustomerPreferences/Console/Command/RefreshStatistics.php
+++ app/code/DvCampus/CustomerPreferences/Console/Command/RefreshStatistics.php
@@ -7,9 +7,35 @@ namespace DvCampus\CustomerPreferences\Console\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\Console\Cli;
+use Magento\Framework\App\Area;
 
 class RefreshStatistics extends \Symfony\Component\Console\Command\Command
 {
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    private $state;
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * RefreshStatistics constructor.
+     * @param \Magento\Framework\App\State $state
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param string|null $name
+     */
+    public function __construct(
+        \Magento\Framework\App\State $state,
+        \Psr\Log\LoggerInterface $logger,
+        string $name = null
+    ) {
+        parent::__construct($name);
+        $this->state = $state;
+        $this->logger = $logger;
+    }
+
     /**
      * @inheritDoc
      */
@@ -31,7 +57,26 @@ class RefreshStatistics extends \Symfony\Component\Console\Command\Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('<info>Running a demo command</info>');
+        try {
+            $this->state->emulateAreaCode(
+                Area::AREA_ADMINHTML,
+                \Closure::fromCallable([$this, 'collectStatistics']),
+                [$output]
+            );
+        } catch (\Exception $e) {
+            $this->logger->critical($e->getMessage());
+
+            return Cli::RETURN_FAILURE;
+        }
+
         return Cli::RETURN_SUCCESS;
+    }
+
+    /**
+     * @param OutputInterface $output
+     */
+    private function collectStatistics(OutputInterface $output): void
+    {
+        $output->writeln('<info>Emulating adminhtml area</info>');
     }
 }
